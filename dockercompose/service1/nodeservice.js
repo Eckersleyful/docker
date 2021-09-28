ef05c97a46171fefc55cae8af71dffede9c49080
Ilmari Saari <ilmari.saari@tuni.fi>
2021-09-28 22:43:49 +0300

    Two node services

---
+++ dockercompose/service1/nodeservice.js
@@ -0,0 +1,34 @@
+var http = require('http')
+var express = require("express");
+var app = express()
+
+
+const options = {
+    hostname: 'localhost',
+    port: 3001,
+    path: '/service2',
+    method: 'GET'
+}
+
+app.listen(8001, () => {
+    console.log("up")
+});
+
+
+app.get("/service1", (req, res, next) => {
+    console.log("Hello from " + req.client.remoteAddress + ":" + req.client.remotePort);
+    console.log("to " + req.client.localAddress + ":" + req.client.localPort);
+    const request = http.request(options, res => {
+        res.on('data', d => {
+            console.log(d)
+        });
+        res.on('error', e => {
+            console.error(e)
+        });
+    });
+    request.on('error', error => {
+        console.error(error)
+    })
+    request.end()
+    res.end()
+});  
---
+++ dockercompose/service2/nodeservice2.js
@@ -0,0 +1,12 @@
+var express = require("express")
+var app = express()
+
+app.listen(3001, () => {
+    console.log("up")
+});
+ 
+app.get("/service2", (req, res, next) => {
+    console.log("Hello from " + req.client.remoteAddress + ":" + req.client.remotePort);
+    console.log("to " + req.client.localAddress + ":" + req.client.localPort);
+    res.status(201)
+});   
